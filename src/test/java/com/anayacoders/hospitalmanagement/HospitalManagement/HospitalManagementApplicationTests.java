2e5188cce03b50190da76577a27bf33943d19a1e
Meet <meetdudhat13@gmail.com>
2025-08-25 16:17:37 +0530

    Project Initialization

---
+++ src/test/java/com/anayacoders/hospitalmanagement/HospitalManagement/HospitalManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anayacoders.hospitalmanagement.HospitalManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
